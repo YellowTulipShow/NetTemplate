131c557d2bfacd9467233a2af4dc63536788d97b
YellowTulipShow <main@yellowtulipshow.site>
2022-06-10 11:55:19 +0800

    运行测试

---
+++ shell/run_test.ps1
@@ -0,0 +1,36 @@
+$ExecutePath = $PWD
+Set-Location $PSScriptRoot
+Set-Location ..
+
+function PrintLineSplit([string]$path)
+{
+    Write-Host ""
+    Write-Host "======================================================================================="
+    Write-Host ""
+}
+
+PrintLineSplit
+
+$template_path = "./_test/template/index.html"
+$output_path = "./_test/output/index.html"
+$db_json_path = "./_test/data/db.json"
+$table_json_path = "./_test/data/table.json"
+
+Write-Host "ctemp -r $PWD -t $template_path -o $output_path --data db:$db_json_path table:$table_json_path"
+ctemp -r $PWD -t $template_path -o $output_path --data db:$db_json_path table:$table_json_path
+
+# 打印输出测试用
+Write-Host "[Template Content]:"
+$content = Get-Content $template_path
+Write-Host $content
+
+Write-Host "[Result Content]:"
+$content = Get-Content $output_path
+Write-Host $content
+
+PrintLineSplit
+
+Set-Location $ExecutePath
+if ($PSScriptRoot -eq $ExecutePath) {
+    timeout.exe /T -1
+}
